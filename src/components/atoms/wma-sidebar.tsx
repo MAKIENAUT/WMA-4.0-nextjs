6f40ba5ef3d759feabac2f866a5daa6f36508c5a
Sly Villafania <villafania.sly.lau@gmail.com>
2024-11-14 14:53:32 +0800

    Create wma-sidebar with shadcn's sidebar components

---
+++ src/components/atoms/wma-sidebar.tsx
@@ -0,0 +1,58 @@
+"use client";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/atoms/ui/sidebar";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
+
+export default function WMASidebar() {
+  const { toggleSidebar } = useSidebar();
+
+  return (
+    <Sidebar side="right">
+      <SidebarHeader>
+        <Button variant="ghost" size="icon" onClick={toggleSidebar}>
+          <X />
+        </Button>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {MENU_ITEMS.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild variant="outline">
+                    <a href={item.url}>{item.title}</a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
+
+const MENU_ITEMS = [
+  {
+    title: "News",
+    url: "/news",
+  },
+  {
+    title: "About Us",
+    url: "/about-us",
+  },
+  {
+    title: "Services",
+    url: "/services",
+  },
+];
