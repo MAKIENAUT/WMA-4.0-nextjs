5e483a939e1bb0dd9427cc3038fd3bfc81d79c6e
Sly Villafania <villafania.sly.lau@gmail.com>
2024-11-15 14:12:48 +0800

    Add Footer to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { SidebarProvider } from "@/components/atoms/ui/sidebar";
+import Footer from "@/components/organisms/footer";
 import Navbar from "@/components/organisms/navbar";
 
 export default function Home() {
   return (
     <SidebarProvider defaultOpen={false}>
       <Navbar />
+      <Footer />
     </SidebarProvider>
   );
 }
