77df5c7221a8737394cbb10af4cdcd12f8f2e2e4
Sly Villafania <villafania.sly.lau@gmail.com>
2024-11-19 22:54:51 +0800

    Move navbar and footer to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { dm_sans } from "@/lib/fonts";
+import { SidebarProvider } from "@/components/atoms/ui/sidebar";
+import Navbar from "@/components/organisms/navbar";
+import Footer from "@/components/organisms/footer";
 
 export const metadata: Metadata = {
   title: "West Migration Agency",
@@ -17,7 +20,11 @@ export default function RootLayout({
       <body
         className={`${dm_sans.className} overflow-x-hidden bg-background antialiased`}
       >
-        {children}
+        <SidebarProvider defaultOpen={false}>
+          <Navbar />
+          {children}
+          <Footer />
+        </SidebarProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
-"use client";
-// pages/index.tsx
-import { SidebarProvider } from "@/components/atoms/ui/sidebar";
-import Footer from "@/components/organisms/footer";
 import HeroSection from "@/components/organisms/hero-section";
-import Navbar from "@/components/organisms/navbar";
 import StatementSection from "@/components/organisms/statement-section";
 import ServiceTemplate from "@/components/templates/ServiceTemplate";
 
 export default function Home() {
   return (
-    <SidebarProvider defaultOpen={false}>
-      <Navbar />
-      <main className="mt-[56px] sm:mt-[72px] md:mt-[80px]">
-        <HeroSection />
-        <StatementSection />
-        <ServiceTemplate />
-      </main>
-      <Footer />
-    </SidebarProvider>
+    <main className="mt-[56px] sm:mt-[72px] md:mt-[80px]">
+      <HeroSection />
+      <StatementSection />
+      <ServiceTemplate />
+    </main>
   );
 }
