02fa748fb9de37716af3e7eb62305bff0c79793d
Sly Villafania <villafania.sly.lau@gmail.com>
2024-11-19 20:24:01 +0800

    Add gradient background to service card

--- src/components/molecules/service-card.tsx
+++ src/components/molecules/service-card.tsx
@@ -20,7 +20,7 @@ export default function ServiceCard({
   return (
     <div className="flex flex-col gap-2">
       <ServicesHeading text={title} level="h2" />
-      <div className="flex flex-col rounded-lg bg-wma-darkTeal sm:flex-row">
+      <div className="flex flex-col rounded-lg bg-gradient-to-r from-wma-darkTeal to-wma-teal sm:flex-row">
         <ServicesImage src={imageUrl} alt={title} />
         <div className="flex size-full flex-col gap-4 p-4 sm:gap-8 sm:overflow-y-scroll lg:p-6">
           <ServicesDescription text={description} />
