89a11a2a34c07a736c4ce2ecdf38561f2efc6456
MAKIENAUT <mcrayescoto@gmail.com>
2024-11-24 12:57:56 +0800

    constraint repair

--- src/app/services/[serviceName]/page.tsx
+++ src/app/services/[serviceName]/page.tsx
@@ -2,16 +2,15 @@ import { notFound } from "next/navigation";
 import ServiceTemplate from "@/components/templates/ServiceTemplate";
 import HeroTemplate from "@/components/templates/HeroTemplate";
 import { immigrationServices } from "@/components/templates/ServiceTemplate";
-
-// Update the interface to use Partial<> with async params
 interface ServicePageProps {
-  params: Partial<{
+  params: {
     serviceName: string;
-  }>;
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }
 
 export default async function ServicePage({
-  params: { serviceName = "" },
+  params: { serviceName },
 }: ServicePageProps) {
   if (!immigrationServices[serviceName]) {
     notFound();
