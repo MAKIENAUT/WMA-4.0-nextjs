cfec7d3e7005fd48611c2bf20bead7dedb7cd992
MAKIENAUT <mcrayescoto@gmail.com>
2024-11-24 13:04:35 +0800

    Assynchronous delete

--- src/app/services/[serviceName]/page.tsx
+++ src/app/services/[serviceName]/page.tsx
@@ -2,14 +2,15 @@ import { notFound } from "next/navigation";
 import ServiceTemplate from "@/components/templates/ServiceTemplate";
 import HeroTemplate from "@/components/templates/HeroTemplate";
 import { immigrationServices } from "@/components/templates/ServiceTemplate";
+
 interface ServicePageProps {
   params: {
     serviceName: string;
   };
-  searchParams?: { [key: string]: string | string[] | undefined };
+  searchParams?: Record<string, string | string[] | undefined>;
 }
 
-export default async function ServicePage({
+export default function ServicePage({
   params: { serviceName },
 }: ServicePageProps) {
   if (!immigrationServices[serviceName]) {
@@ -23,9 +24,3 @@ export default async function ServicePage({
     </>
   );
 }
-
-// export async function generateStaticParams() {
-//   return Object.keys(immigrationServices).map((serviceName) => ({
-//     serviceName,
-//   }));
-// }
