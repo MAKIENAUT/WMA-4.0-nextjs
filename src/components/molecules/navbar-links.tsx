b4a28aecab8d6b0a1f0c0b33d02b90ecc4c31300
Sly Villafania <villafania.sly.lau@gmail.com>
2024-11-28 19:04:43 +0800

    Update menu and sidebar to have sign in button

--- src/components/molecules/navbar-links.tsx
+++ src/components/molecules/navbar-links.tsx
@@ -1,14 +1,41 @@
+import { ChevronDown } from "lucide-react";
 import { Button } from "../atoms/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../atoms/ui/dropdown-menu";
 import { NavbarProps } from "../organisms/navbar";
+import Link from "next/link";
 
 export default function NavbarLinks({ datas }: NavbarProps) {
   return (
     <ul className="hidden md:inline-flex md:gap-4">
       {datas.map((data) => (
-        <Button key={data.title} asChild variant="link">
-          <a href={data.url}>{data.title}</a>
-        </Button>
+        <li key={data.title}>
+          <Button asChild variant="link">
+            <Link href={data.url}>{data.title}</Link>
+          </Button>
+        </li>
       ))}
+      <li>
+        <Button variant="link">
+          <Link href="/login">Sign in</Link>
+        </Button>
+      </li>
+      <li className="hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="font-medium">
+              Account <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent side="bottom">
+            <DropdownMenuItem>Sign out</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </li>
     </ul>
   );
 }
